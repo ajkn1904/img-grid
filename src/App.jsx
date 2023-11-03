c4e284ee27867ca2acf8d13e08c0ee6f5ab56676
ajkn1904 <ajknishat@gmail.com>
2023-11-03 22:55:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Home from './components/home'
+
+function App() {
+  return (
+    <div className='bg-gray-200 p-3'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='bg-white h-[95vh] rounded-md p-5'>
+            <div>
+                <h1 className='text-3xl'>Gallery</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
