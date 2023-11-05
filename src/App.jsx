02d8a508d505c0734cf832c4b93d5e5a4db1c82b
ajkn1904 <ajknishat@gmail.com>
2023-11-05 21:50:03 +0600

    delete functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './components/home'
 
 function App() {
   return (
-    <div className='bg-gray-200 p-3'>
+    <div className='bg-gray-200 p-3 lg:p-5'>
       <Home />
     </div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -64,8 +64,10 @@ const Home = () => {
         e.dataTransfer.setData('index', i);
     };
 
+    const newItemsList = [...items];
+
     const onDrop = (e, i) => {
-        const newItemsList = [...items];
+        //const newItemsList = [...items];
         const currentIndex = e.dataTransfer.getData('index');
         const [draggedImg] = newItemsList.splice(currentIndex, 1);
         newItemsList.splice(i, 0, draggedImg);
@@ -73,37 +75,55 @@ const Home = () => {
     };
 
     const toggleSelection = (i) => {
-        const newItemsList = [...items];
+        //const newItemsList = [...items];
         newItemsList[i].selected = !newItemsList[i].selected;
         setItems(newItemsList);
     };
 
+    const filteredList = newItemsList.filter(data => data.selected);
+    //console.log(filteredList);
+
+    const deleteSelectedItems = () => {
+        const updatedList = newItemsList.filter(data => !data.selected);
+        setItems(updatedList);
+    };
+    console.log(filteredList);
 
     return (
-        <div className='bg-white rounded-md p-5'>
+        <div className='bg-white rounded-md py-5 md:py-10'>
 
-            <div className='pb-3 border-b-2'>
-                <h1 className='text-3xl'>Gallery</h1>
+            <div className='px-5 md:px-10 pb-3 border-b-2'>
+                <h1 className={`text-3xl pt-4 md:pt-0 pb-[17px] ${filteredList.length !== 0 ? 'hidden' : ''}`}>Gallery</h1>
+
+                <div className={`flex justify-between items-center pt-4 md:pt-0 pb-[3.4px] ${filteredList.length === 0 ? 'hidden' : ''}`}>
+                    <div className='flex items-center gap-2'>
+                        <input type="checkbox" className='h-4 w-4 md:h-5 md:w-5' checked readOnly />
+                        <p className='font-semibold text-lg'>{filteredList.length} {filteredList.length === 1 ? 'File' : 'Files'} Selected</p>
+                    </div>
+
+                    <button onClick={deleteSelectedItems} className='text-red-600 text-lg bg-white hover:bg-red-50 border-none'>Delete {filteredList.length === 1 ? 'file' : 'files'} </button>
+                </div>
             </div>
 
 
-            <div className='grid grid-cols-5 grid-rows-3 gap-3 mt-5'>
+            <div className='grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 gap-5 px-5 md:px-10 my-5'>
 
                 {items.map((item, index) => {
                     return (
-                        
-                        <div key={index} onDragOver={e => e.preventDefault()} onDragStart={e => onDragStart(e, index)} onDrop={e => onDrop(e, index)} draggable='true' onClick={() => toggleSelection(index)} className={`${index === 0 ? 'row-span-2 col-span-2' : ''} border rounded-md cursor-pointer hover:border-2 relative`}>
-                         
-                         <div className="relative group">
-                                
-                                <input type='checkbox' checked={item.selected} className={`absolute top-2 right-2 z-10 opacity-0 ${item.selected ? 'opacity-100' : 'group-hover:opacity-100'}`} readOnly />
-                                
+
+                        <div key={index} onDragOver={e => e.preventDefault()} onDragStart={e => onDragStart(e, index)} onDrop={e => onDrop(e, index)} draggable='true' onClick={() => toggleSelection(index)} className={`${index === 0 ? 'row-span-2 col-span-2' : ''} border rounded-md cursor-pointer hover:border-none hover:shadow-md hover:shadow-blue-300 relative`}>
+
+                            <div className="relative group">
+
+                                <input type='checkbox' checked={item.selected} className={`h-4 w-4 md:h-5 md:w-5 absolute top-4 right-4 z-10 opacity-0 ${item.selected ? 'opacity-100' : 'group-hover:opacity-100'}`} readOnly />
+
                                 <img src={item.image} alt="image" className={`rounded-md transition-transform transform bg-white hover:brightness-50 ${item.selected ? 'brightness-90' : ''}`} />
-                            
+
                             </div>
 
                         </div>
-                    )}
+                    )
+                }
                 )}
 
             </div>
